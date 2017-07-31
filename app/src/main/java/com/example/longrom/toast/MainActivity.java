c513cfb3d9be3c6d314eab64cb0fc27cd667cbd1
= <=>
2017-07-31 09:44:42 +0700

    add code

---
+++ app/src/main/java/com/example/longrom/toast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.longrom.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnToast;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnToast = (Button) findViewById(R.id.btnToast);
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
